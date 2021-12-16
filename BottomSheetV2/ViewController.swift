a57301e58e1094673b9d4c4833d7729ca32ff364
mariusaee <mariusae@me.com>
2021-12-16 15:36:39 +0300

    Initial Commit

---
+++ BottomSheetV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BottomSheetV2
+//
+//  Created by Marius Malyshev on 16.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
